a29b77e0dc9956bef6828b2c457d8e5b1f33e598
joty <jotyprokash61@gmail.com>
2026-03-23 16:26:38 +0600

    Add VPC module for base networking

---
+++ Deployment POC/Terraform-IaC-POC-3/modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
