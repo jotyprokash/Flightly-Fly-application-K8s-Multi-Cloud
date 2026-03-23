5182e434361684eac7a121b2c78355861a36c508
Joty Mondal <111188117+jotyprokash@users.noreply.github.com>
2026-03-24 01:25:38 +0600

    refactoring deploy_app.sh

--- terraform/deploy_app.sh
+++ terraform/deploy_app.sh
@@ -9,28 +9,28 @@ DB_ENDPOINT=$5
 DB_USER=${DB_USER:-flightlyadmin}
 DB_PASSWORD=${DB_PASSWORD:-SecureFlightlyPassword123!}
 
-echo "🚀 [1/7] Authenticating Docker with AWS ECR..."
+echo "[1/7] Authenticating Docker with AWS ECR..."
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $(echo $FRONTEND_ECR | cut -d'/' -f1)
 
 export REGISTRY_URL=$(echo $FRONTEND_ECR | cut -d'/' -f1)
 
-echo "🏗️ [2/7] Building and Pushing Backend Docker Image..."
+echo "[2/7] Building and Pushing Backend Docker Image..."
 cd ../backend
 docker build -t flightly-backend .
 docker tag flightly-backend:latest $BACKEND_ECR:latest
 docker push $BACKEND_ECR:latest
 
-echo "🏗️ [3/7] Building and Pushing Frontend Docker Image..."
+echo "[3/7] Building and Pushing Frontend Docker Image..."
 cd ../frontend
 docker build -t flightly-frontend .
 docker tag flightly-frontend:latest $FRONTEND_ECR:latest
 docker push $FRONTEND_ECR:latest
 cd ../terraform
 
-echo "🔐 [4/7] Updating local Kubeconfig..."
+echo "[4/7] Updating local Kubeconfig..."
 aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME
 
-echo "🔑 [5/7] Re-seeding DocumentDB Credentials safely into EKS..."
+echo "[5/7] Re-seeding DocumentDB Credentials safely into EKS..."
 kubectl create secret generic flightly-db-secret \
   --from-literal=MONGO_URI="mongodb://${DB_USER}:${DB_PASSWORD}@${DB_ENDPOINT}:27017/?tls=false&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false" \
   --dry-run=client -o yaml | kubectl apply -f -
@@ -55,11 +55,11 @@ helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller \
   --set region=$REGION \
   --set vpcId=$(aws eks describe-cluster --name $CLUSTER_NAME --region $REGION --query "cluster.resourcesVpcConfig.vpcId" --output text)
 
-echo "⏳ Waiting for ALB Controller pods to be ready..."
+echo "Waiting for ALB Controller pods to be ready..."
 kubectl rollout status deployment aws-load-balancer-controller -n kube-system --timeout=120s
 
-echo "🎯 [7/7] Applying application via Kustomize Overlays..."
+echo "[7/7] Applying application via Kustomize Overlays..."
 cd ../
 kubectl apply -k k8s/overlays/production
 
-echo "✅ Deployment Process Finished! The Load Balancer and Domain are waking up..."
+echo "Deployment Process Finished! The Load Balancer and Domain are waking up..."
