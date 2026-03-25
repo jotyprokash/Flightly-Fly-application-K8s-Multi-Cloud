d50e60ac61e5477d6e2deaf67297fd42946fb573
joty <jotyprokash61@gmail.com>
2026-03-26 00:13:40 +0600

    Configure IAM OIDC provider and update EKS outputs for Helm/K8s integration

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -7,3 +7,18 @@ output "cluster_endpoint" {
   description = "The endpoint for your Kubernetes API server"
   value       = aws_eks_cluster.main.endpoint
 }
+
+output "cluster_certificate_authority_data" {
+  description = "The certificate authority data for the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "oidc_provider_url" {
+  description = "The URL of the OIDC provider"
+  value       = aws_iam_openid_connect_provider.main.url
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC provider"
+  value       = aws_iam_openid_connect_provider.main.arn
+}
