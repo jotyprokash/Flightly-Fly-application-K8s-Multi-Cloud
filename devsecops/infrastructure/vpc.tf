a73e4835e5fb6338b42401993a87136bd8e7454d
jotyprokash <jotyprokash61@gmail.com>
2026-04-02 15:13:03 +0600

    create core networking layer for devsecops cluster

---
+++ devsecops/infrastructure/vpc.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "devsecops_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "flightly-devsecops-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devsecops_vpc.id
+
+  tags = {
+    Name = "flightly-devsecops-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.devsecops_vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 1)
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+
+  tags = {
+    Name = "flightly-devsecops-public"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.devsecops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "flightly-devsecops-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
